1c25e44c88ab27473c142a6a7a478984afd8be75
Evinek <evinek95@gmail.com>
2018-01-29 14:57:09 +0100

    multi-files example for Scala

---
+++ languages/39/examples/multifiles_prog.scala
@@ -0,0 +1,5 @@
+object Main extends App {
+    println("first")
+    val second = new Second()
+    second.print
+}
---
+++ languages/39/examples/multifiles_prog2.scala
@@ -0,0 +1,5 @@
+class Second {
+    def print {
+        println("second")
+    }
+}
